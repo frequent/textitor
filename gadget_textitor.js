5a387f3f00fcc32fdec48164d3be8eb979ad3378
Sven Franck <svenfranck@yahoo.com>
2016-03-17 09:36:24 +0100

    debug: make sure storage is available to following promise steps

--- gadget_textitor.js
+++ gadget_textitor.js
@@ -19,6 +19,7 @@
     .declareMethod('render', function (my_option_dict) {
       var gadget = this,
         return_gadget;
+
       return new RSVP.Queue()
         .push(function () {
           return RSVP.all([
@@ -43,22 +44,25 @@
               });
             })
             .push(function (my_storage) {
-              return my_storage.put("textitor");
-            })
-            .push(function (my_id) {
-              console.log(my_id);
-              return my_storage.putAttachment(
-                my_id, 
-                "http://foo.css", 
-                new Blob(["span%2C%20div%20%7Bborder%3A%201px%20solid%20red%20!important%3B%7D"], {
-                  type: "text/css",
+              return new RSVP.Queue()
+                .push(function () {
+                  return my_storage.put("textitor");
                 })
-              );
-            })
-            .push(function (my_response) {
-              console.log("done");
-              console.log(my_response);
-              return return_gadget;
+                .push(function (my_id) {
+                  console.log(my_id);
+                  return my_storage.putAttachment(
+                    my_id, 
+                    "http://foo.css", 
+                    new Blob(["span%2C%20div%20%7Bborder%3A%201px%20solid%20red%20!important%3B%7D"], {
+                      type: "text/css",
+                    })
+                  );
+                })
+                .push(function (my_response) {
+                  console.log("done");
+                  console.log(my_response);
+                  return return_gadget;
+                });
             });
         });
     });
