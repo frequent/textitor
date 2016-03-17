2d41b61b2b70d16302259b2feb88c51775a5b081
Sven Franck <svenfranck@yahoo.com>
2016-03-17 09:14:36 +0100

    fix: remove RSVP.js from service worker

--- serviceworker.js
+++ serviceworker.js
@@ -8,7 +8,7 @@
 // importScripts('./serviceworker-cache-polyfill.js');
 
 // make RSVP the Promise of choice
-importScripts('./rsvp.js');
+// importScripts('./rsvp.js');
 
 // https://developer.mozilla.org/en-US/docs/Web/API/Service_Worker_API/Using_Service_Workers
 // http://www.html5rocks.com/en/tutorials/service-worker/introduction/
