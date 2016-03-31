aa121bad22386ae675ef3073c22e7c3129792476
Sven Franck <svenfranck@yahoo.com>
2016-03-31 06:23:47 +0200

    cleanup and fail on purpose to throw on higher level

--- gadget_serviceworker.js
+++ gadget_serviceworker.js
@@ -19,26 +19,17 @@
     .declareMethod('render', function (my_option_dict) {
       return this;
     })
-    
+
     .declareMethod('passRequest', function (my_method, my_param_list) {
       var gadget = this;
-      console.log("passing to jio");
       return new RSVP.Queue()
         .push(function () {
           return gadget.getDeclaredGadget("jio_gadget");
         })
         .push(function (my_jio_gadget) {
-          console.log(my_jio_gadget);
-          console.log("calling with");
-          console.log(my_method);
-          console.log(my_param_list);
-          console.log([].concat(my_param_list))
-          return my_jio_gadget[my_method].apply(my_jio_gadget, [].concat(my_param_list));
-        })
-        .push(undefined, function (error) {
-          throw error;
+          //return my_jio_gadget[my_method].apply(my_jio_gadget, [].concat(my_param_list));
+          return my_jio_gadget[my_method].apply(my_jio_gadget, my_param_list);
         });
     });
-    
 
 }(window, rJS));
