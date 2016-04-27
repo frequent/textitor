d35ee7ff3ab9a153122e769a81135fb253a738f6
Sven Franck <svenfranck@yahoo.com>
2016-04-27 22:51:12 +0200

    remove console, make serviceworker call fallback

--- gadget_codemirror.js
+++ gadget_codemirror.js
@@ -299,25 +299,26 @@
         active_cache = CodeMirror.menu_dict.active_cache || "textitor";
         return new RSVP.Queue()
           .push(function () {
-            console.log("setting memory");
             return my_gadget.setActiveStorage("memory");
           })
           .push(function () {
             return my_gadget.jio_getAttachment(active_cache, file_name);
           })
           .push(function (my_result) {
+            consolt.log("RESPONSE")
             console.log(my_result);
           })
-          .push(undefined, function (my_error) {
-            console.log(my_error);
-            throw my_error;
-          })
-          .push(function () {
-            console.log("setting serviceworker");
-            return my_gadget.setActiveStorage("serviceworker");
-          })
-          .push(function () {
-            return my_gadget.jio_getAttachment(active_cache, file_name);
+          .push(null, function (my_error) {
+             if ((error instanceof jIO.util.jIOError) &&
+              (error.status_code === 404)) {
+                return new RSVP.Queue()
+                  .push(function () {
+                    return my_gadget.setActiveStorage("serviceworker");
+                  })
+                  .push(function () {
+                    return my_gadget.jio_getAttachment(active_cache, file_name);
+                  });
+              }
           })
           .push(function (my_response) {
             my_gadget.property_dict.editor.setOption("mode", my_response.type);
