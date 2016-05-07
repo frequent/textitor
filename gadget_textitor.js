cef412ea43300b45349bf7012ea0aa69dadc9bb8
Sven Franck <svenfranck@yahoo.com>
2016-05-07 17:37:05 +0200

    debug: console which storage is being used

--- gadget_textitor.js
+++ gadget_textitor.js
@@ -102,7 +102,6 @@
     })
     
     .declareMethod('setActiveStorage', function (my_type) {
-      console.log("setting active to: " + my_type);
       this.property_dict.storage_dict.active = my_type;
     })
     
@@ -110,9 +109,7 @@
       var gadget = this,
         dict = gadget.property_dict,
         active_storage = dict.storage_dict[dict.active || "serviceworker"];
-      
-      console.log("routing");
-      console.log(active_storage);
+      console.log("running request on: " + active_storage.state_parameter_dict.label)
       return active_storage[my_method].apply(
         active_storage,
         [].concat(my_param_list)
