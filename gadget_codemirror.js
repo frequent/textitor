b18ae310a8487fc1321f80fa4c8c54c7970eaf21
Sven Franck <svenfranck@yahoo.com>
2016-06-19 01:19:33 +0200

    cleanup

--- gadget_codemirror.js
+++ gadget_codemirror.js
@@ -486,7 +486,6 @@
       // XXX: missing create cache
       if (!is_cache_name) {
         mime_type_input = file_name.split(".").pop().replace("/", "");
-        console.log(mime_type_input)
         mime_type = modeMimes[mime_type_input] ||
             modeMimes[shimModeMimes[mime_type_input]] ||
                 "text/plain";
