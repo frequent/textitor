0c0a5116593ce3af0c416a6c9b2c13df6ffbbc87
Sven Franck <svenfranck@yahoo.com>
2016-06-22 13:21:02 +0200

    set filename to passed value or active file is there is one

--- gadget_codemirror.js
+++ gadget_codemirror.js
@@ -343,9 +343,9 @@
         console.log(file_name)
 
         // set modified if it's a file which was not saved before
-        //if (file_name.indexOf("*") > 0) {
+        if (file_name.indexOf("*") > 0) {
         //  CodeMirror.menu_dict.editor_setModified();
-        //}
+        }
 
         // drop the star
         file_name = file_name.split("*")[0];
@@ -692,8 +692,9 @@
           console.log("option or active file?")
           console.log(my_option_dict.value)
           console.log(editor_getActiveFile()[0])
-          //text_input.value = my_option_dict.value || editor_getActiveFile()[0];
-          text_input.value = my_option_dict.value;
+          console.log("setting form input to whatever is active")
+          text_input.value = my_option_dict.value || editor_getActiveFile()[0];
+          //text_input.value = my_option_dict.value;
           if (my_option_dict.selectValueOnOpen !== false) {
             text_input.select();
           }
