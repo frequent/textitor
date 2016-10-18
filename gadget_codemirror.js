a3313b8d732986b3e588a18c408ba92d04e89353
Sven Franck <svenfranck@yahoo.com>
2016-10-18 14:15:41 +0200

    debug: console

--- gadget_codemirror.js
+++ gadget_codemirror.js
@@ -926,12 +926,10 @@
         return;
       }
       if (!props.editor_active_file) {
-        console.log(dialog)
-        console.log(dialog.querySelector("input[type='text']"))
+        file_name_input = dialog.querySelector("input");
         console.log(file_name_input)
-        console.log(file_name_input.value)
-        file_name_input = dialog.querySelector("input[type='text']") || {};
-        file_name = file_name_input.value || "";
+        
+        file_name = file_name_input.value;
         is_cache_name = dialog.querySelector('input:checked');
         mime_type_input = file_name.split(".").pop().replace("/", "");
         mime_type = MODEMIMES[mime_type_input] ||
