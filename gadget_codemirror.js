f67e92acd8e531190a77e86a49a54cdbc1150fbe
Sven Franck <svenfranck@yahoo.com>
2016-11-12 13:55:02 +0100

    debug: console

--- gadget_codemirror.js
+++ gadget_codemirror.js
@@ -703,11 +703,14 @@
     }
     if (position === my_direction) {
       if (position === "left" && props.editor_active_path) {
-        console.log("position and direction are left, if active path, go up", props.editor_active_path)
+        console.log("position and direction are left, active_path", props.editor_active_path)
         //props.editor_active_path = path_list.splice(path_list.length, -1, 1).join("/") || null;
         path_list = props.editor_active_path.split("/");
+        console.log(path_list)
         path_list.splice(path_list.length - 1, 1).join("/");
+        console.log(path_list)
         props.editor_active_path = path_list || null;
+        console.log("editor active_path, ", props.editor_active_path)
         props.editor_setDisplay(props.editor.active_path)
         parameter = BLANK_SEARCH;
       } else {
